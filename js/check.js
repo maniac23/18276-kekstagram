4b13d4430b8d4cabc67d382950a3d3e710eb7c60
Mikhail Tukai <tukaym@gmail.com>
2016-01-21 23:08:19 +0300

    created functions multyplyArrays and sumArrays

--- js/check.js
+++ js/check.js
@@ -1,29 +1,44 @@
-var getMessage = function(a, b){ // функция для подсчета статистики по разным типам изображений
-	if(typeof(a)=='boolean'){
-		if(a){
+var multyplyArrays = function(a, b) { // функция для перемножения элементов двух массивов
+  var result = 0;
+
+	for(var i=0, l = a.length; i < l; i++){
+		result += a[i] * b[i];
+	}
+	
+	return result;
+}
+
+var sumArrays= function(a,b){    // функция для суммирования элементов массива
+	var sum=0;
+	
+	for(var x=0; x<a.length; x++){ 
+		sum+=a[x];
+	}
+	return sum;
+}
+
+var getMessage = function(a, b) { // функция для подсчета статистики по разным типам изображений
+	if (typeof(a)=='boolean') {
+		if (a) {
 			return "Переданное GIF-изображение анимировано и содержит " + b + " кадров";			 
-		}
+		} 
 		else {
 			return "Переданное GIF-изображение не анимировано";
 		}
-	}
-	else if(typeof(a)=='number'){
+	} 
+	else if (typeof(a)=='number') {
 		return "Переданное SVG-изображение содержит " + a + " объектов и " + b * 4 + " аттрибутов";	
-	}
-	else if(typeof(a) =='object'){
-		if(typeof(b) == 'object'){
-			square = 0;
-			for(var i=0; i<a.length; i++){
-				square += a[i]*b[i];
-			}
-			return  "Общая площадь артефактов сжатия: " + square + " пикселей";
-		}
+	} 
+	else if (typeof(a) =='object') {
+		if (typeof(b) == 'object') {
+			return  "Общая площадь артефактов сжатия: " + multyplyArrays(a, b) + " пикселей";
+		} 
 		else {
 			 sum=0;
-			for(var x=0; x<a.length; x++){
+			for(var x=0; x<a.length; x++) {
 				sum+=a[x];
 			}
-			return "Количество красных точек во всех строчках изображения: "+ sum;
+			return "Количество красных точек во всех строчках изображения: "+ sumArrays(a,b);
 		}
 	}
 }	
