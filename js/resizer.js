aa0ab1feec39d09b260bff7a03ce47fe428f4acc
Mikhail Tukai <tukaym@gmail.com>
2016-01-24 18:38:45 +0300

    исправил форматирование кода

--- js/resizer.js
+++ js/resizer.js
@@ -133,21 +133,16 @@
 
       // Отрисовка прямоугольника, обозначающего область изображения после
       // кадрирования. Координаты задаются от центра.
-      
       this._ctx.strokeRect(
           (-this._resizeConstraint.side / 2) - this._ctx.lineWidth / 2,
           (-this._resizeConstraint.side / 2) - this._ctx.lineWidth / 2,
           this._resizeConstraint.side - this._ctx.lineWidth / 2,
           this._resizeConstraint.side - this._ctx.lineWidth / 2);
-
-
-     //fiilText 
-      this._ctx.font = "20px Open Sans";
-      this._ctx.fillStyle = "#fff";
+      //fiilText
+      this._ctx.font = '20px Open Sans';
+      this._ctx.fillStyle = '#fff';
       this._ctx.textAlign = 'center';
-      this._ctx.fillText(this._image.naturalWidth + " x " + this._image.naturalHeight, 0, (-this._resizeConstraint.side / 2) - (this._ctx.lineWidth + this._ctx.lineWidth / 2));
-     
-
+      this._ctx.fillText(this._image.naturalWidth + ' x ' + this._image.naturalHeight, 0, (-this._resizeConstraint.side / 2) - (this._ctx.lineWidth + this._ctx.lineWidth / 2));
       // Восстановление состояния канваса, которое было до вызова ctx.save
       // и последующего изменения системы координат. Нужно для того, чтобы
       // следующий кадр рисовался с привычной системой координат, где точка
@@ -155,7 +150,6 @@
       // некорректно сработает даже очистка холста или нужно будет использовать
       // сложные рассчеты для координат прямоугольника, который нужно очистить.
       this._ctx.restore();
-
     },
 
     /**
