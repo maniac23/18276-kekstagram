887f13cb80ecca8deb1969fa3a611bef28a6446a
Mikhail Tukai <tukaym@gmail.com>
2016-02-26 16:46:21 +0300

    добавил jsDoc в файл check.js, отформатировал

--- js/check.js
+++ js/check.js
@@ -1,43 +1,46 @@
+'use strict';
 define(function() {
-var multyplyArrays = function(a, b) { // функция для перемножения элементов двух массивов
-  var result = 0;
+  /**
+   * Функция генерирует сообщенние  о загруженном фото  в зависимости
+   * от его расширения
+   * @paran {*} a - параметр типа фото
+   * @param  {*=} b -  параметр типа фото
+   * @returns {string} -  сообщение о формате загруженного фото
+  */
+  var multyplyArrays = function(a, b) { // функция для перемножения элементов двух массивов
+    var result = 0;
 
-  for(var i=0, l = a.length; i < l; i++){
-    result += a[i] * b[i];
-  }
+    for (var i = 0, l = a.length; i < l; i++) {
+      result += a[i] * b[i];
+    }
 
-  return result;
-}
+    return result;
+  };
 
-var sumArrays= function(a,b){    // функция для суммирования элементов массива
-	var sum=0;
+  var sumArrays = function(a) {    // функция для суммирования элементов массива
+    var sum = 0;
+    for (var i = 0; i < a.length; i++) {
+      sum += a[i];
+    }
+    return sum;
+  };
 
-	for(var i=0; i<a.length; i++){
-		sum+=a[i];
-	}
-	return sum;
-}
-
-var getMessage = function(a, b) { // функция для подсчета статистики по разным типам изображений
-	if (typeof(a)=='boolean') {
-		if (a) {
-			return "Переданное GIF-изображение анимировано и содержит " + b + " кадров";
-		}
-		else {
-			return "Переданное GIF-изображение не анимировано";
-		}
-	}
-	else if (typeof(a)=='number') {
-		return "Переданное SVG-изображение содержит " + a + " объектов и " + b * 4 + " аттрибутов";
-	}
-	else if (typeof(a) =='object') {
-		if (typeof(b) == 'object') {
-			return  "Общая площадь артефактов сжатия: " + multyplyArrays(a, b) + " пикселей";
-		}
-		else {
-			return "Количество красных точек во всех строчках изображения: "+ sumArrays(a,b);
-		}
-	}
-}
+  var getMessage = function(a, b) { // функция для подсчета статистики по разным типам изображений
+    if (typeof (a) === 'boolean') {
+      if (a) {
+        return 'Переданное GIF-изображение анимировано и содержит ' + b + ' кадров';
+      } else {
+        return 'Переданное GIF-изображение не анимировано';
+      }
+    } else if (typeof (a) === 'number') {
+      return 'Переданное SVG-изображение содержит ' + a + ' объектов и ' + b * 4 + ' аттрибутов';
+    } else if (typeof (a) === 'object') {
+      if (typeof (b) === 'object') {
+        return 'Общая площадь артефактов сжатия: ' + multyplyArrays(a, b) + ' пикселей';
+      } else {
+        return 'Количество красных точек во всех строчках изображения: ' + sumArrays(a, b);
+      }
+    }
+  };
 });
 
